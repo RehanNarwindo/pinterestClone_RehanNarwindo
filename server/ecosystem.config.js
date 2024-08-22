62454f1f2dd0c46bad67e76cfc8660f94230ab07
Rehan Aji Narwindo <151915916+RehanNarwindo@users.noreply.github.com>
2024-08-22 14:13:13 +0700

    Update ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,16 +1,1 @@
-module.exports = {
-  apps: [
-    {
-      name: "app",
-      script: "./index.js",
-      env: {
-        PORT: 80,
-        NODE_ENV: "production",
-        JWT_SECRET: "RAHASIA",
-        MONGGO_DB_URI:
-          "mongodb+srv://rehanajinarwindo:Startmeup22@cluster0.ssg5qzo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-        REDIS_PW: "pVJ6VwfLCCYd9jXtouG4y9Bj0cN4Wzwv",
-      },
-    },
-  ],
-};
+
